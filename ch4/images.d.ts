be9a98931447e5d8a88bc5b04cd1411f46cf5a1e
sehun jeong <80654807+jeongsehun0102@users.noreply.github.com>
2025-06-09 11:11:48 +0900

    Add files via upload

--- ch4/images.d.ts
+++ ch4/images.d.ts
@@ -1,21 +1,13 @@
-// images.d.ts
+// CH4/images.d.ts (또는 declarations.d.ts)
 
 declare module '*.png' {
-  const value: import('react-native').ImageSourcePropType;
-  export default value;
-}
-
-declare module '*.jpg' {
-  const value: import('react-native').ImageSourcePropType;
-  export default value;
-}
-
-declare module '*.jpeg' { // jpeg도 추가
-  const value: import('react-native').ImageSourcePropType;
-  export default value;
-}
-
-declare module '*.gif' { // gif 추가
-  const value: import('react-native').ImageSourcePropType;
-  export default value;
-} 
+    const value: import('react-native').ImageSourcePropType;
+    export default value;
+  }
+  
+  declare module '*.jpg' {
+    const value: import('react-native').ImageSourcePropType;
+    export default value;
+  }
+  
+  // 필요시 다른 이미지 타입(gif, jpeg 등)도 추가
